790f685019f4412476791c0056183dcdab75ce23
gwn <dev@gwn.wtf>
2021-01-21 23:13:04 +0300

    Switch to the socket API from the REST API

--- index.js
+++ index.js
@@ -1,117 +1,174 @@
 const
     debug = require('debug')('ixo-assistant-ui-lite'),
-    React = require('react'),
-    {Fragment, useState, useEffect, useCallback} = React
+    io = require('socket.io-client'),
+    {createElement: e, Fragment, useState, useEffect, useCallback, useRef}
+        = require('react')
 
 
 const defaultComponents = {
     TextMessage: ({direction, text}) =>
-        <div>{{in: '<', out: '>'}[direction]} {text}</div>,
+        e('div', null, {in: '<', out: '>'}[direction] + ' ' + text),
 
-    Button: props => <button {...props} />,
+    Button: props => e('button', props),
 
-    Input: props => <input {...props} />,
+    Input: ({onChangeText, ...props}) =>
+        e('input', {...props, onChange: e => onChangeText(e.target.value)}),
 }
 
 module.exports = ({
-    rasaUrl,
-    sender,
+    rasaSocket,
+    userId,
+    initialSessionId,
     initialMessage,
-    onCustomResponse,
+    onCustomResponse = () => null,
+    onError = () => null,
     components: {
         TextMessage = defaultComponents.TextMessage,
         Button = defaultComponents.Button,
         Input = defaultComponents.Input,
-    },
+    } = defaultComponents,
 }) => {
     const
+        sockRef = useRef(null),
+        sessionIdRef = useRef(null),
+
         [msgDraft, setMsgDraft] = useState(''),
-        [msgHistory, setMsgHistory] =
-            useState([{
-                ts: Date.now(),
-                direction: 'out',
-                text: initialMessage,
-            }])
 
-    const pushMsgToHistory = useCallback(msg =>
-        setMsgHistory(lastMsgHistory =>
-            [...lastMsgHistory, {ts: Date.now(), ...msg}]))
+        [msgHistory, setMsgHistory] = useState([]),
 
-    const removeMsgFromHistory = useCallback(msgIdx =>
-        setMsgHistory(lastMsgHistory => [
-            ...lastMsgHistory.slice(0, msgIdx),
-            ...lastMsgHistory.slice(msgIdx + 1),
-        ]))
+        pushMsgToHistory = useCallback(
+            msg =>
+                setMsgHistory(lastMsgHistory =>
+                    [...lastMsgHistory, {ts: Date.now(), ...msg}])),
 
-    const msgAssistant = useCallback(async msg =>
-        (await msgRasa(rasaUrl, sender, msg)).body.forEach(handleIncoming))
+        removeMsgFromHistory = useCallback(
+            msgIdx =>
+                setMsgHistory(lastMsgHistory => [
+                    ...lastMsgHistory.slice(0, msgIdx),
+                    ...lastMsgHistory.slice(msgIdx + 1),
+                ])),
 
-    const handleIncoming = useCallback(msg => {
-        if (msg.text)
-            pushMsgToHistory({direction: 'in', text: msg.text})
+        msgAssistant = useCallback(
+            message => {
+                debug('sending msg', message)
 
-        if (msg.buttons)
-            pushMsgToHistory({direction: 'in', buttons: msg.buttons})
+                sockRef.current.emit('user_uttered', {
+                    session_id: sessionIdRef.current,
+                    message,
+                })
+            },
 
-        if (msg.custom)
-            onCustomResponse(msg.custom, pushMsgToHistory)
-    })
+            [sockRef.current, sessionIdRef.current],
+        ),
 
-    useEffect(() => void msgAssistant(initialMessage), [])
+        restartSession = useCallback(
+            (sock, sessionId) =>
+                sockRef.current.emit('session_request', {
+                    session_id: sessionId || String(Date.now()) + userId,
+                }),
+        ),
 
-    return <>
-        {msgHistory.map((msg, msgIdx) => {
+        handleIncoming = useCallback(
+            msg => {
+                if (msg.text)
+                    pushMsgToHistory({direction: 'in', text: msg.text})
+
+                if (msg.quick_replies)
+                    pushMsgToHistory({
+                        direction: 'in', quick_replies: msg.quick_replies})
+
+                if (msg.buttons)
+                    pushMsgToHistory({direction: 'in', buttons: msg.buttons})
+
+                if (!msg.text && !msg.quick_replies && !msg.buttons)
+                    onCustomResponse(msg, pushMsgToHistory)
+            })
+
+    useEffect(() => {
+        const
+            {url: sockUrl, ...sockOpts} = rasaSocket,
+            sock = io(sockUrl, sockOpts)
+
+        sockRef.current = sock
+
+        sock
+            .on('connect', () => restartSession(sock, initialSessionId))
+
+            .on('connect_error', e => {
+                debug('connect_error', e)
+                onError(e)
+            })
+
+            .on('disconnect', e => console.info('disconnect', e))
+
+            .on('session_confirm', sessInfo => {
+                debug('session confirmed', sessInfo)
+
+                sessionIdRef.current = sessInfo.session_id
+
+                setMsgHistory([])
+
+                if (initialMessage) {
+                    msgAssistant(initialMessage)
+                    pushMsgToHistory({direction: 'out', text: initialMessage})
+                }
+            })
+
+            .on('bot_uttered', msg => {
+                debug('bot_uttered', msg)
+                handleIncoming(msg)
+            })
+
+            .on('error', e => {
+                debug('error', e)
+                onError(e)
+            })
+    }, [])
+
+    return [
+        ...msgHistory.map((msg, msgIdx) => {
             if (msg.text)
-                return <TextMessage key={msg.ts} {...msg} />
+                return e(TextMessage, {key: msg.ts, ...msg})
 
-            if (msg.buttons)
-                return msg.buttons.map(btn =>
-                    <Button
-                        key={msg.ts + btn.payload}
-                        children={btn.title}
-                        onClick={() => {
+            if ((msg.quick_replies || msg.buttons))
+                return (msg.quick_replies || msg.buttons).map(btn =>
+                    e(Button, {
+                        key: msg.ts + btn.payload,
+                        children: btn.title,
+                        onClick: () => {
                             msgAssistant(btn.payload)
-                            removeMsgFromHistory(msgIdx)
                             pushMsgToHistory({direction: 'out',text: btn.title})
-                        }}
-                    />)
+
+                            if (msg.quick_replies)
+                                removeMsgFromHistory(msgIdx)
+                        },
+                    })
+                )
 
             if (msg.component)
-                return <Fragment key={msg.ts}>{msg.component}</Fragment>
-        })}
+                return e(Fragment, {key: msg.ts, children: msg.component})
+        }),
 
-        <Input value={msgDraft} onChange={setMsgDraft} />
+        e(Input, {
+            key: 'msgInput',
+            value: msgDraft,
+            onChangeText: setMsgDraft,
+        }),
 
-        <Button
-            onClick={() => {
+        e(Button, {
+            key: 'sendBtn',
+            onClick: () => {
                 msgAssistant(msgDraft)
                 pushMsgToHistory({direction: 'out', text: msgDraft})
                 setMsgDraft('')
-            }}
-            children='Send'
-        />
-    </>
-}
+            },
+            children: 'Send',
+        }),
 
-const msgRasa = async (rasaUrl, sender, message) => {
-    const
-        url = rasaUrl,
-        opts = {
-            method: 'POST',
-            body: JSON.stringify({sender, message}),
-        }
-
-    debug('> Request', url, opts)
-
-    const
-        resp = await fetch(url, opts),
-        body = await resp.json()
-
-    debug('< Response', {status: resp.status, headers: resp.headers, body})
-
-    return Promise[resp.ok ? 'resolve' : 'reject']({
-        status: resp.status,
-        headers: resp.headers,
-        body,
-    })
+        e(Button, {
+            key: 'restartBtn',
+            onClick: restartSession,
+            children: 'Restart',
+        }),
+    ]
 }
